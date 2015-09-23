16cee4c17642218bb689fcb6d3d102312d6f361a
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2015-09-23 16:48:51 +0200

    Rename file

---
+++ getTweets.py
@@ -0,0 +1,53 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler, Cursor, Stream, API, TweepError
+import json
+
+consumer_key = 'consumer_key'
+consumer_secret = 'consumer_secret'
+access_token = 'access_token'
+access_token_secret = 'access_token_secret'
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+
+        def on_error(self, error_code):
+            print error_code
+            return True
+
+        def on_timeout(self):
+            print 'Timeout!'
+            return True
+
+        def on_disconnect(self):
+            print 'Disconnected'
+            return True
+
+            
+def streamTweets(keywords, languages):
+    listener = StdOutListener()
+    authentication = OAuthHandler(consumer_key, consumer_secret)
+    authentication.set_access_token(access_token, access_token_secret)
+    stream = Stream(authentication, listener)
+    stream.filter(track=keywords, languages=languages)
+
+def findTweets(keywords):
+    authentication = OAuthHandler(consumer_key, consumer_secret)
+    authentication.set_access_token(access_token, access_token_secret)
+    api = API(authentication)
+    cursor = Cursor(api.search, q=keywords).items()
+    while True:
+        try:
+            tweet = cursor.next()
+            print ','.join([str(tweet.created_at), tweet.lang, tweet.text]).encode('utf-8')
+except TweepError:
+    time.sleep(60 * 15)
+    continue
+except StopIteration:
+    break
+
+if __name__== "__main__":
+    keywords=['na', 'w', 'o', 'na', 'to', 'z', 'i', 'do', 'nie', 'to', 'lub']
+    languages=['pl']
+    streamTweets(keywords, languages)
